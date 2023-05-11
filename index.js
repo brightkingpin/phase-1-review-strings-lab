a3e8f72b9636a0253688ef65ac7ac281b1756350
brightkingpin <luvandabright781@gmail.com>
2023-05-11 21:24:40 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-// My code
-const currentUser = "Grace Hopper";
-
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-
+// My Modified code.
+const currentUser = 'Grace Hopper';
+const welcomeMessage =`Welcome to Flatbook, ' + ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
 
 
 
